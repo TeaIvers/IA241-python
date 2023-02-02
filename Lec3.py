f37dbbd93206536698a4a87f2bb1467b86dcb2ca
EC2 Default User <ec2-user@ip-172-31-31-175.ec2.internal>
2023-02-02 21:46:28 +0000

    added my lab3.py file.

---
+++ Lec3.py
@@ -0,0 +1,16 @@
+'''
+lec3
+list and set
+'''
+my_list = [1,2,3,4,5] 
+print(type(my_list))
+my_nested_list= [1,2,3,my_list] 
+print(my_nested_list)
+my_list[0]=6
+print(my_list)
+print(my_list[0])
+print(my_list[:3])
+x,y,z= ['a','b','c'] 
+print(x,y,z) 
+print(my_list +[8,9])
+
---
+++ lab3.py
@@ -0,0 +1,31 @@
+'''
+lab 3
+'''
+str_list= ['a','d','e','b','c']
+str_list.sort() 
+print(str_list)
+str_list.append('f')
+print(str_list)
+str_list.remove('d')
+print(str_list)
+print(str_list[2])
+my_list=['a','123',123,'b','B','False',False,123,None,'None']
+print(my_list)
+print(len(set(my_list)))
+x="This is my third python lab.".split(" ")
+print(len(x))
+num_list=[12,32,43,35]
+num_list.sort()
+print(num_list)
+print(num_list[-1])
+print(num_list[0])
+game_board=[[0,0,0],[0,0,0],[0,0,0]] 
+print(game_board)
+game_board[1][1]=1
+print(game_board)
+
+
+
+
+
+
