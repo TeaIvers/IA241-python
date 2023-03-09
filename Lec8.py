a43826d6cf93225f39ddaedc250610a13f15ef1c
EC2 Default User <ec2-user@ip-172-31-31-175.ec2.internal>
2023-03-09 21:25:38 +0000

    lab 8 define

---
+++ Lec8.py
@@ -0,0 +1,34 @@
+'''
+def lec 8
+'''
+
+def cal_plus(input1,input2=0):
+    return input1+input2
+    print(input1)
+    print(input2)
+    result= input1+input2
+    return result
+
+print(cal_plus(2))
+def cal_sigma(m,n):
+    #return m+n
+    result= 0
+    for i in range(n,m+1):
+       result= result+i
+    return result
+print(cal_sigma(5,3))
+
+def cal_pi(m,n):
+    result = 1
+    for i in range (n,m+1):
+       result = result *1
+    print(result)
+    
+    
+       
+    
+    
+    
+    
+    
+    
